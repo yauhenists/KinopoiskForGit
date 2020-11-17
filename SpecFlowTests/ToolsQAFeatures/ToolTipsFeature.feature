11a85f7f143e9b7f268058512f69f16df3764842
Yauheni Stetukevich <y.stsetsiukevich@itechart-group.com>
2020-11-17 18:28:13 +0300

    Specflow - Tooltips test

---
+++ SpecFlowTests/ToolsQAFeatures/ToolTipsFeature.feature
@@ -0,0 +1,8 @@
+﻿Feature: ToolTipsFeature
+	Simple check of tooltip text
+
+@mytag
+Scenario: Check tooltip text
+	Given I have opened tooltips page
+	When I hover mouse over button
+	Then I see tooltip with text You hovered over the Button
---
+++ SpecFlowTests/ToolsQASteps/TooltipsSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BoDi;
+using KinopoiskSelenium;
+using KinopoiskSelenium.Pages.ToolsQA;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTests.ToolsQASteps
+{
+    [Binding]
+    public sealed class TooltipsSteps
+    {
+        private readonly IObjectContainer _container;
+        private ConciseApi _conciseApi;
+        private ToolTipsPage _page;
+
+        public TooltipsSteps(IObjectContainer container)
+        {
+            _container = container;
+            _conciseApi = _container.Resolve<ConciseApi>();
+        }
+
+        [Given(@"I have opened tooltips page")]
+        public void GivenIHaveOpenedTooltipsPage()
+        {
+            _page = new ToolTipsPage(_conciseApi);
+        }
+
+        [When(@"I hover mouse over button")]
+        public void WhenIHoverMouseOverButton()
+        {
+            _page.MoveCursorOnTooltipButton();
+        }
+
+        [Then(@"I see tooltip with text (.*)")]
+        public void ThenISeeTooltipWithText(string tooltipText)
+        {
+            StringAssert.AreEqualIgnoringCase(tooltipText, _page.GetTooltipText());
+        }
+
+    }
+}
