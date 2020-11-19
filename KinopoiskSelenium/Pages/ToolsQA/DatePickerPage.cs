988cdb6f56e4aef066a9d8e3b2c43e3a3aa230cd
Yauheni Stetukevich <y.stsetsiukevihc@itechart-group.com>
2020-11-19 22:21:56 +0300

    DatePicker Selenium test 1

---
+++ KinopoiskSelenium/Pages/ToolsQA/DatePickerPage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace KinopoiskSelenium.Pages.ToolsQA
+{
+    public class DatePickerPage : BasePage
+    {
+        private string _url = "https://demoqa.com/date-picker";
+        private By _selectDateField = By.Id("datePickerMonthYearInput");
+
+        private By _datePickerPrevious = By.XPath("//button[text()='Previous Month']");
+        private By _datePickerNext = By.XPath("//button[text()='Next Month']");
+
+        private By _datePickerCurrentMontYear =
+            By.XPath("//*[contains(concat(' ',@class,' '), 'current-month--hasMonthDropdown')]");
+        public DatePickerPage(ConciseApi conciseApi) : base(conciseApi)
+        {
+            OpenPage();
+        }
+
+        public override void OpenPage()
+        {
+            ConciseApi.Open(_url);
+        }
+
+        public void OpenDatePicker()
+        {
+            ConciseApi.Click(_selectDateField);
+        }
+
+        public void GetPreviousMonth(bool previous = true)
+        {
+            if (previous)
+            {
+                ConciseApi.Click(_datePickerPrevious);
+            }
+            else
+            {
+                ConciseApi.Click(_datePickerNext);
+            }
+        }
+
+        public string GetDateFromDatePicker()
+        {
+            return ConciseApi.GetTextOfTheElement(_datePickerCurrentMontYear);
+        }
+    }
+}
---
+++ KinopoiskSelenium/Tests/ToolsQA/DatePickerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KinopoiskSelenium.Pages.ToolsQA;
+using NUnit.Framework;
+
+namespace KinopoiskSelenium.Tests.ToolsQA
+{
+    [TestFixture]
+    public class DatePickerTests : BaseTest
+    {
+        [Test]
+        public void CheckMonthYearInDatePicker()
+        {
+            DatePickerPage page = new DatePickerPage(ConciseApi);
+            page.OpenDatePicker();
+            Assert.AreEqual(DateTime.Now.ToString("MMMM yyyy", CultureInfo.GetCultureInfo("en-en")), page.GetDateFromDatePicker());
+            page.GetPreviousMonth();
+            Assert.AreEqual(DateTime.Now.AddMonths(-1).ToString("MMMM yyyy", CultureInfo.GetCultureInfo("en-en")), page.GetDateFromDatePicker());
+        }
+    }
+}
